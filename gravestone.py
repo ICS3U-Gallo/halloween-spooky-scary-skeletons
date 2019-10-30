5cd23da85e28df0c4b336e28b5139d99695f98bf
cng04 <54898144+cng04@users.noreply.github.com>
2019-10-30 19:54:13 -0400

    Update gravestone.py

--- gravestone.py
+++ gravestone.py
@@ -3,6 +3,12 @@ import arcade
 
 WIDTH = 640
 HEIGHT = 480
+X1 = WIDTH - 400
+Y1 = HEIGHT // 2 - 20
+X2 = X1 + 75
+Y2 = Y1
+X3 = X2 + 25
+Y3 = Y2
 
 window = arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
 
@@ -21,27 +27,27 @@ def update(delta_time):
 def on_draw():
     arcade.start_render()
 # Gravestone
-    arcade.draw_rectangle_filled(WIDTH // 2, HEIGHT // 4, 200, 250, arcade.color.LIGHT_GRAY, 180)
-    arcade.draw_ellipse_filled(WIDTH // 2, 230, 100, 100, arcade.color.LIGHT_GRAY, 180, 120)
+    arcade.draw_rectangle_filled(WIDTH // 2, HEIGHT // 4, WIDTH // 3 - 13, (HEIGHT + 20) // 2, arcade.color.LIGHT_GRAY,180)
+    arcade.draw_ellipse_filled(WIDTH // 2, HEIGHT // 2 - 10, 200, 200, arcade.color.LIGHT_GRAY, 180, 120)
 # Cross
-    arcade.draw_rectangle_filled(WIDTH // 2, 390, 20, 120, arcade.color.GRAY, 0)
-    arcade.draw_rectangle_filled(WIDTH // 2, 400, 20, 80, arcade.color.GRAY, 90)
-# RIP 
+    arcade.draw_rectangle_filled(WIDTH // 2, HEIGHT - 90, 20, 120, arcade.color.GRAY, 0)
+    arcade.draw_rectangle_filled(WIDTH // 2, HEIGHT - 80, 20, 80, arcade.color.GRAY, 90)
+# RIP
     # R
-    arcade.draw_line(240, 220, 240, 120, arcade.color.BLACK, 1)
-    arcade.draw_line(240, 220, 290, 220, arcade.color.BLACK, 1)
-    arcade.draw_line(290, 220, 290, 180, arcade.color.BLACK, 1)
-    arcade.draw_line(240, 180, 290, 180, arcade.color.BLACK, 1)
-    arcade.draw_line(240, 180, 290, 120, arcade.color.BLACK, 1)
+    arcade.draw_line(X1, Y1, X1, Y1 - 100, arcade.color.BLACK, 5)
+    arcade.draw_line(X1, Y1, X1 + 50, Y1, arcade.color.BLACK, 5)
+    arcade.draw_line(X1 + 50, Y1, X1 + 50, Y1 - 40, arcade.color.BLACK, 5)
+    arcade.draw_line(X1, Y1 - 40, X1 + 50, Y1 - 40, arcade.color.BLACK, 5)
+    arcade.draw_line(X1, Y1 - 40, X1 + 50, Y1 - 100, arcade.color.BLACK, 5)
 
     # I
-    arcade.draw_line(315, 220, 315, 120, arcade.color.BLACK, 1 )
+    arcade.draw_line(X2, Y2, X2, Y2 - 100, arcade.color.BLACK, 5)
 
-    # P 
-    arcade.draw_line(340, 220, 340, 120, arcade.color.BLACK, 1)
-    arcade.draw_line(340, 220, 390, 220, arcade.color.BLACK, 1)
-    arcade.draw_line(390, 220, 390, 180, arcade.color.BLACK, 1)
-    arcade.draw_line(340, 180, 390, 180, arcade.color.BLACK, 1)
+    # P
+    arcade.draw_line(X3, Y3, X3, Y3 - 100, arcade.color.BLACK, 5)
+    arcade.draw_line(X3, Y3, X3 + 50, Y3, arcade.color.BLACK, 5)
+    arcade.draw_line(X3 + 50, Y3, X3 + 50, Y3 - 40, arcade.color.BLACK, 5)
+    arcade.draw_line(X3, Y3 - 40, X3 + 50, Y3 - 40, arcade.color.BLACK, 5)
 @window.event
 def on_key_press(key, modifiers):
     pass
