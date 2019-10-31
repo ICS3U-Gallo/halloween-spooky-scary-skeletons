a36ac3d810e24317a7190162159ac14b6d112275
cng04 <54898144+cng04@users.noreply.github.com>
2019-10-30 20:23:01 -0400

    Update gravestone(updated).py

--- gravestone(updated).py
+++ gravestone(updated).py
@@ -3,12 +3,10 @@ import arcade
 
 WIDTH = 640
 HEIGHT = 480
-X1 = WIDTH - 400
-Y1 = HEIGHT // 2 - 20
-X2 = X1 + 75
-Y2 = Y1
-X3 = X2 + 25
-Y3 = Y2
+X1 = WIDTH // 2 - 57
+Y1 = HEIGHT // 2 - 50
+
+
 
 window = arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
 
@@ -33,21 +31,8 @@ def on_draw():
     arcade.draw_rectangle_filled(WIDTH // 2, HEIGHT - 90, 20, 120, arcade.color.GRAY, 0)
     arcade.draw_rectangle_filled(WIDTH // 2, HEIGHT - 80, 20, 80, arcade.color.GRAY, 90)
 # RIP
-    # R
-    arcade.draw_line(X1, Y1, X1, Y1 - 100, arcade.color.BLACK, 5)
-    arcade.draw_line(X1, Y1, X1 + 50, Y1, arcade.color.BLACK, 5)
-    arcade.draw_line(X1 + 50, Y1, X1 + 50, Y1 - 40, arcade.color.BLACK, 5)
-    arcade.draw_line(X1, Y1 - 40, X1 + 50, Y1 - 40, arcade.color.BLACK, 5)
-    arcade.draw_line(X1, Y1 - 40, X1 + 50, Y1 - 100, arcade.color.BLACK, 5)
-
-    # I
-    arcade.draw_line(X2, Y2, X2, Y2 - 100, arcade.color.BLACK, 5)
-
-    # P
-    arcade.draw_line(X3, Y3, X3, Y3 - 100, arcade.color.BLACK, 5)
-    arcade.draw_line(X3, Y3, X3 + 50, Y3, arcade.color.BLACK, 5)
-    arcade.draw_line(X3 + 50, Y3, X3 + 50, Y3 - 40, arcade.color.BLACK, 5)
-    arcade.draw_line(X3, Y3 - 40, X3 + 50, Y3 - 40, arcade.color.BLACK, 5)
+    arcade.draw_text("R.I.P.", X1, Y1, arcade.color.BLACK, 50)
+    
 @window.event
 def on_key_press(key, modifiers):
     pass
